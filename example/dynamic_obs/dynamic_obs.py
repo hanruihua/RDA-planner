f4233025a2be9cba80a60b0c5f643d078d32b4d1
hanruihua <744475118@qq.com>
2023-03-16 21:23:31 +0800

    test dyna

--- example/dynamic_obs/dynamic_obs.py
+++ example/dynamic_obs/dynamic_obs.py
@@ -6,11 +6,11 @@ from collections import namedtuple
 import time
 
 # environment
-env = EnvBase('path_track.yaml', save_ani=False, display=True, full=False)
+env = EnvBase('dynamic_obs.yaml', save_ani=False, display=True, full=False)
 car = namedtuple('car', 'G h cone_type wheelbase max_speed max_acce')
 
 # saved ref path
-npy_path = sys.path[0] + '/path_track_ref.npy'
+npy_path = sys.path[0] + '/dynamic_obs.npy'
 ref_path_list = list(np.load(npy_path, allow_pickle=True))
 env.draw_trajectory(ref_path_list, traj_type='-k') # plot path
 
@@ -19,7 +19,9 @@ def main():
     robot_info = env.get_robot_info()
     car_tuple = car(robot_info.G, robot_info.h, robot_info.cone_type, robot_info.shape[2], [10, 1], [10, 0.5])
     
-    mpc_opt = MPC(car_tuple, ref_path_list, receding=10, sample_time=env.step_time, process_num=4, iter_num=3, ro1=100)
+    obstacle_template_list = [{'edge_num': 3, 'obstacle_num': 2, 'cone_type': 'norm2'}, {'edge_num': 4, 'obstacle_num': 0, 'cone_type': 'Rpositive'}]  # define the number of obstacles in advance
+
+    mpc_opt = MPC(car_tuple, ref_path_list, receding=10, sample_time=env.step_time, process_num=4, iter_num=3, ro1=100, obstacle_template_list=obstacle_template_list)
     
     for i in range(500):   
         
