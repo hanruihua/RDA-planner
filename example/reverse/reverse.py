e01532b1f6ee0439b89c3c43dcce0b75224e5d33
hanruihua <744475118@qq.com>
2023-03-23 17:23:29 +0800

    update reserve

--- example/reverse/reverse.py
+++ example/reverse/reverse.py
@@ -4,19 +4,17 @@ from RDA_planner.mpc import MPC
 from collections import namedtuple
 from GCT.curve_generator import curve_generator
 
+env = EnvBase('reverse.yaml', save_ani=False, display=True, full=False)
+
 # start and goal point of the robot
 point1 = np.array([ [5], [40], [0]])
 point2 = np.array([ [35], [11], [-3.14]])
 point3 = np.array([ [43.8], [11], [-3.14]])
-point_list = [point1, point2, point3]
+point_list = [env.robot.state[0:3], point1, point2, point3]
 
 cg = curve_generator()
 ref_path_list = cg.generate_curve('reeds', point_list, 0.5, 5, include_gear=True)
 
-robot_init_point = np.zeros((4, 1))
-robot_init_point[0:3] = ref_path_list[0][0:3]
-
-env = EnvBase('reverse.yaml', save_ani=False, display=True, full=False, robot_args={'state': robot_init_point, 'goal':point3})
 car = namedtuple('car', 'G h cone_type wheelbase max_speed max_acce')
 
 env.draw_trajectory(ref_path_list, traj_type='-k')
