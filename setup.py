3f78501b3977c654d42138d302333bd97e18c6a6
Han <hanrh@connect.hku.hk>
2024-05-07 18:28:13 +0800

    version 1.3

--- setup.py
+++ setup.py
@@ -4,13 +4,14 @@ import sys
 setup(
     name='RDA_planner',
     py_modules=['RDA_planner'],
-    version= '1.2',
+    version= '1.3',
     install_requires=[
         'cvxpy',
         'numpy',
         'pathos',
-        'ir_sim==1.1.9',
-        'matplotlib'
+        'ir_sim==1.1.11',
+        'matplotlib',
+        'gctl==1.1'
     ],
     description="The source code of optimization based RDA motion planner",
     author="Han Ruihua"
