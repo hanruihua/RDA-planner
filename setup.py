917eafd0d054b3101d5d4e6b3bd8763c5c89a451
han <744475118@qq.com>
2024-09-21 11:10:03 +0800

    change for irsim 2.2.4

--- setup.py
+++ setup.py
@@ -8,11 +8,7 @@ setup(
         'cvxpy==1.5.2',
         'numpy',
         'pathos',
-<<<<<<< HEAD
-        'ir-sim==2.2.3',
-=======
-        'ir_sim==2.1.4',
->>>>>>> change for irsim 2.2.4
+        'ir_sim==2.2.4',
         'matplotlib',
         'gctl==1.1',
         'opencv-python',
