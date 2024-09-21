a9fdf77ec1d0b14d6a9fd49d509bf3d0e41a6a70
han <744475118@qq.com>
2024-09-22 01:23:11 +0800

    python version

--- setup.py
+++ setup.py
@@ -3,14 +3,15 @@ from setuptools import setup
 setup(
     name='RDA_planner',
     py_modules=['RDA_planner'],
-    version= '2.3',
+    version= '2.4',
+    python_requires='>=3.8',
     install_requires=[
         'cvxpy==1.5.2',
         'numpy',
         'pathos',
         'ir_sim==2.2.4',
         'matplotlib',
-        'gctl==1.1',
+        'gctl==1.2',
         'opencv-python',
         'imageio',
         'scikit-learn',
